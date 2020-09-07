f2f5b57d4b9316be598095e05be1694539e356df
polenykes <polenykes@gmail.com>
2020-09-07 18:50:08 +1200

    fix up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-//https://challonge.com/ENZSFVW415
-
-//zU8uSiEtYLjlqFXPQCRzto3RiEp1KegdJ8TCyvLj
-//i shouldn't need to even provide this for public info? check that.
-
-
-//ENZWFT7PCSEPTQ2
-//hard code for tonight. 07/09/2020
-
 //global for lazy people
 var participants = [];
 
@@ -35,25 +26,14 @@ let getCompletedMatches = (tournament) => new Promise(function (resolve, reject)
     });
 });
 
-//ENZBossBattle //big test
-//ac1ieajq //my test
-
-//ENZWFT7PCSEPTQ2
-//hard code for tonight. 07/09/2020
-
 const go = () => {
-    console.log('gooo');
     let bracket = $("#bracket").val();
-    console.log('going with', bracket);
+    console.log('going with bracket: ', bracket);
 
     if(bracket){
         Promise.all([getCompletedMatches(bracket)]).then(function (values) {
-            console.log('new results pulled.');
             let matches = values[0].matches;
             participants = values[0].participants;
-
-            console.log(matches);
-            console.log(participants);
             
             let completed = matches.filter(match => match.match.state == "complete");
             let maxCompletedWinnersRound = Math.max.apply(Math, completed.map((o) => { return o.match.round; }));
@@ -65,7 +45,6 @@ const go = () => {
                 let unfinishedWinners = matches.filter(
                     match => match.match.state != "complete" && match.match.round == maxCompletedWinnersRound
                 );
-                console.log(unfinishedWinners);
                 if (unfinishedWinners.length >= 1) {
                     roundsToShow.push(maxCompletedWinnersRound);
                     roundsToShow.push(maxCompletedWinnersRound - 1);
@@ -90,7 +69,7 @@ const go = () => {
                 roundsToShow.push(-1);
             }
 
-            console.log('roundsToShow', roundsToShow);
+            console.log('Rounds To Display', roundsToShow);
 
             //matches to display
             let display = matches.filter(match =>
@@ -103,16 +82,13 @@ const go = () => {
 
             var i = 0;
             const loopDisplay = () =>  {
-                    
-                    console.log('loopin ', i);
-                    $("#round").text(display[i].match.round > 0 ? `winners round ${display[i].match.round}` : `losers round ${Math.abs(display[i].match.round)}`);
+                    $("#round").text(display[i].match.round > 0 ? `Winners round ${display[i].match.round}` : `Losers round ${Math.abs(display[i].match.round)}`);
                     $("#match").html(`${convertIdToName(display[i].match.winner_id)} <span class="subtext">defeats</span> ${convertIdToName(display[i].match.loser_id)}`);
                     i++;
                     setTimeout(function () {
                         if (i < display.length) {
                             loopDisplay();
-                        }else{
-                            //start again 
+                        }else{ 
                             go();
                         }
                 }, 5000)
@@ -127,20 +103,15 @@ go();
 
 
 const convertIdToName = (id) => {
-    console.log(id);
-
     let matchedParticipant = participants.find(participant => participant.participant.id == id || participant.participant.group_player_ids.includes(id));
-    console.log(matchedParticipant);
     return matchedParticipant.participant.display_name;
 }
 
 
 $("#round").on('click', () => {
-    console.log('tog');
     $("#bracket").toggle();
 });
 
 $("#bracket").on('change', () => {
-    console.log('change');
     go();
 });
